345c50e4a7af19d9728c899277959b0755c459dc
Tatiana <vikora@land.ru>
2020-11-16 16:22:25 +0300

    Some formatting

--- lcm.cpp
+++ lcm.cpp
@@ -12,13 +12,11 @@ int gcd(int a, int b) {
     return a > b ? a : b;
 }
 
-
 long long lcm_naive(int a, int b) {
-  for (long l = 1; l <= (long long) a * b; ++l)
-    if (l % a == 0 && l % b == 0)
-      return l;
-
-  return (long long) a * b;
+    for (long l = 1; l <= (long long) a * b; ++l)
+        if (l % a == 0 && l % b == 0)
+            return l;
+    return (long long) a * b;
 }
 
 long long lcm_fast(int a, int b) {
@@ -44,10 +42,10 @@ void test_solution(int a, int b) {
 }
 
 int main() {
-  int a, b;
-  std::cin >> a >> b;
-  //std::cout << lcm_naive(a, b) << std::endl;
-  std::cout << lcm_fast(a, b) << std::endl;
-  //test_solution(a, b);
-  return 0;
+    int a, b;
+    std::cin >> a >> b;
+    //std::cout << lcm_naive(a, b) << std::endl;
+    std::cout << lcm_fast(a, b) << std::endl;
+    //test_solution(a, b);
+    return 0;
 }
