ec097f16cc322a901db21c33e67eaef0972480c4
Tatiana <vikora@land.ru>
2020-11-16 16:54:04 +0300

    Compose the largest number out of a set of integers

---
+++ largest_number.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::vector;
+using std::string;
+
+string largest_number(vector<string> a) {
+  std::stringstream ret;
+
+  std::sort(a.begin(), a.end(),
+    [](const string &x, const string &y) -> bool {
+      int xy = std::stoi(x + y), yx = std::stoi(y + x);
+      return xy > yx;
+    }
+  );
+
+  for (size_t i = 0; i < a.size(); i++) {
+    ret << a[i];
+  }
+  string result;
+  ret >> result;
+  return result;
+}
+
+int main() {
+  int n;
+  std::cin >> n;
+  vector<string> a(n);
+  for (size_t i = 0; i < a.size(); i++) {
+    std::cin >> a[i];
+  }
+  std::cout << largest_number(a);
+}
